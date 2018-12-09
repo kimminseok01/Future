b1669d84af46aa49e706d520f4f828477568b008
kimminseok01 <45648041+kimminseok01@users.noreply.github.com>
2018-12-09 13:04:22 +0900

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  4 16:49:41 2018
+
+@author: Ed432
+"""
+
+import time
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn import svm, metrics
+from sklearn.model_selection import train_test_split
+
+def start():
+    start_time = time.time()
+    return start_time
+
+def end(start_time):
+    return time.time() - start_time
+
+
+df = pd.read_csv('final_data_1.csv')
+
+x = df.drop('label', axis = 1)
+x = x.drop('patient',axis = 1)
+y = df['label']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+
+start_time = start()
+svmc = svm.SVC()
+svmc.fit(x_train, y_train)
+pred_1 = svmc.predict(x_test)
+end1 = end(start_time)
+
+start_time = start()
+rfc = RandomForestClassifier(n_estimators=1000)
+rfc.fit(x_train, y_train)
+pred_2 = rfc.predict(x_test)
+end2 = end(start_time)
+
+start_time = start()
+gbc = GradientBoostingClassifier(n_estimators=1000)
+gbc.fit(x_train, y_train)
+pred_3 = gbc.predict(x_test)
+end3 = end(start_time)
+
+
+accuracy_1 = metrics.accuracy_score(y_test, pred_1)
+report_1 = metrics.classification_report(y_test, pred_1)
+
+accuracy_2 = metrics.accuracy_score(y_test, pred_2)
+report_2 = metrics.classification_report(y_test, pred_2)
+
+accuracy_3 = metrics.accuracy_score(y_test, pred_3)
+report_3 = metrics.classification_report(y_test, pred_3)
+
+print()
+print('Time of supporting vector machine: ', end1)
+print('Time of random forest: ', end2)
+print('Time of gradient boosting: ', end3)
+print()
+print('Accuracy of supporting vector machine: ', accuracy_1)
+print('Accuracy of random forest: ', accuracy_2)
+print('Accuracy of gradient boosting: ', accuracy_3)
+print()
+
+print('Supporting Vector Machine'.center(70),'\n',report_1)
+print()
+print('Random Forest'.center(70),'\n',report_2)
+print()
+print('Gradient Boosting'.center(70),'\n',report_3)
